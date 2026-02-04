80a0f8e32f7743005f9e999e1e137b9d6227b438
sk9o9 <sumitkhadka098@gmail.com>
2026-02-04 11:23:58 +0545

    fetched all the harrypoter movies name and images

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.render("index.ejs");
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const API_URL = "https://api.potterdb.com/v1/movies";
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL);
+    res.render("index.ejs", { movies: result.data.data, error: null });
+  } catch (err) {
+    res.render("index.ejs", {
+      movies: null,
+      error: "Failed to fetch movies",
+    });
+  }
 });
 
 app.listen(port, () => {
